72758d89261ccc5bacea291875997f92ad3d4f58
Henry Holtgeerts <henryholtgeerts@gmail.com>
2021-05-18 19:22:10 -0400

    feature: load block assets

--- src/blocks.js
+++ src/blocks.js
@@ -9,4 +9,5 @@
  * Webpack is compiling as the input file.
  */
 
-import './block/block.js';
+import './hyperblock/block.js';
+import './hyperchild/block.js';
--- src/init.php
+++ src/init.php
@@ -27,10 +27,10 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @uses {wp-editor} for WP editor styles.
  * @since 1.0.0
  */
-function hyperblock_cgb_block_assets() { // phpcs:ignore
+function hyper_assets() { // phpcs:ignore
 	// Register block styles for both frontend + backend.
 	wp_register_style(
-		'hyperblock-cgb-style-css', // Handle.
+		'hyper-css', // Handle.
 		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
 		is_admin() ? array( 'wp-editor' ) : null, // Dependency to include the CSS after it.
 		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
@@ -38,7 +38,7 @@ function hyperblock_cgb_block_assets() { // phpcs:ignore
 
 	// Register block editor script for backend.
 	wp_register_script(
-		'hyperblock-cgb-block-js', // Handle.
+		'hyper-editor-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
 		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
@@ -47,7 +47,7 @@ function hyperblock_cgb_block_assets() { // phpcs:ignore
 
 	// Register block editor styles for backend.
 	wp_register_style(
-		'hyperblock-cgb-block-editor-css', // Handle.
+		'hyper-editor-css', // Handle.
 		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
 		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
 		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
@@ -55,7 +55,7 @@ function hyperblock_cgb_block_assets() { // phpcs:ignore
 
 	// WP Localized globals. Use dynamic PHP stuff in JavaScript via `cgbGlobal` object.
 	wp_localize_script(
-		'hyperblock-cgb-block-js',
+		'hyper-editor-js',
 		'cgbGlobal', // Array containing dynamic data for a JS Global.
 		[
 			'pluginDirPath' => plugin_dir_path( __DIR__ ),
@@ -75,16 +75,16 @@ function hyperblock_cgb_block_assets() { // phpcs:ignore
 	 * @since 1.16.0
 	 */
 	register_block_type(
-		'cgb/block-hyperblock', array(
+		'hyper/hyperblock', array(
 			// Enqueue blocks.style.build.css on both frontend & backend.
-			'style'         => 'hyperblock-cgb-style-css',
+			'style'         => 'hyper-css',
 			// Enqueue blocks.build.js in the editor only.
-			'editor_script' => 'hyperblock-cgb-block-js',
+			'editor_script' => 'hyper-editor-js',
 			// Enqueue blocks.editor.build.css in the editor only.
-			'editor_style'  => 'hyperblock-cgb-block-editor-css',
+			'editor_style'  => 'hyper-editor-css',
 		)
 	);
 }
 
 // Hook: Block assets.
-add_action( 'init', 'hyperblock_cgb_block_assets' );
+add_action( 'init', 'hyper_assets' );
